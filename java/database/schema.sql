5e00520ab1a389cb01dd10410fbecdfeedcae060
manakassa <87282021+manakassa@users.noreply.github.com>
2021-08-04 15:19:14 -0400

    BeersTable

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,7 +1,8 @@
- bBEGIN TRANSACTION;
+ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS breweries;
+DROP TABLE IF EXISTS beers;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -39,5 +40,21 @@ INSERT INTO breweries (brewery_name, brewery_state, brewery_city, brewery_addres
 INSERT INTO breweries (brewery_name, brewery_state, brewery_city, brewery_address, brewery_zip_code, brewery_phone_number, brewery_website_url, description, image_url) VALUES ('BrewDog Brewing Company, LLC', 'OH', 'Canal Winchester', '96 Gender Rd', '43110', '(614) 908-3051', 'http://www.brewdog.com/usa', 'BrewDog was born with the aim to revolutionize the beer industry and completely redefine beer-drinking culture. We’re determined to make a stand for independence, a stand for quality and stand for craft.', 'https://www.brewdog.com/images/cms/blog/1534839878BLOG1.jpg');
 INSERT INTO breweries (brewery_name, brewery_state, brewery_city, brewery_address, brewery_zip_code, brewery_phone_number, brewery_website_url, description, image_url) VALUES ('Homestead Beer Co.', 'OH', 'Heath', '811 Irving Wick Dr W', '43056', '(740) 522-8018', 'www.homesteadbeerco.com',  'Enjoy the flavors of Ohio through our lifelong committment to excellence. On the Homestead-- We know beer.', 'https://untappd.akamaized.net/site/brewery_logos_hd/brewery-9935_12ab5_hd.jpeg');
 
+
+CREATE TABLE beers (
+beer_id serial primary key,
+beer_name varchar(50) not null,
+brewery_id int not null,
+beer_image varchar(500),
+
+constraint fk_beers_brewery_id foreign key (brewery_id) references breweries(brewery_id)
+);
+
+INSERT INTO beers (beer_name , brewery_id , beer_image )
+VALUES ('Samuel Adams' , 1 , 'https://boeningbrothers.com/product/samuel-adams-jack-o/' );
+INSERT INTO beers (beer_name , brewery_id , beer_image )
+VALUES ('Punk IPA' , 2 , 'https://www.drinksandco.com/beer/brewdog-punk-ipa' );
+INSERT INTO beers (beer_name , brewery_id , beer_image )
+VALUES ('St George Amber Beer' , 3 , 'https://beerisdevine.com/wp-content/uploads/BGI-Ethiopia-St-George-Beer-678x381.jpg' );
 
 COMMIT TRANSACTION;
