63e450f5feb3e923bf751639d46b5a016a9c29d4
Tyler Knapp <tylerknapp25@gnmail.com[D[D[D[D[D[D[D[[C[C[C[C[C[Cmail.com>
2021-08-06 11:15:06 -0400

    Beer Detail and click event functionally working

--- vue/src/components/BeerCard.vue
+++ vue/src/components/BeerCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="beerCard" >
+  <div id="beerCard" v-on:click="goToBeerPage">
       <h1 class="name">{{beer.name}}</h1>
       <img class="beerimage" v-bind:src="beer.imageUrl" >
   </div>
@@ -8,10 +8,32 @@
 <script>
 export default {
     props: ["beer"],
+    methods: {
+      goToBeerPage(){
+      this.$router.push({
+        name: "beer-detail",
+        params: {
+          
+          beerID: this.beer.beerId
+        }
+      })
+    }
+    }
 
 }
 </script>
 
 <style>
+#beerCard{
+ display: flex;
+  flex-direction: column;
+  margin: 20px;
+  padding: 10px;
+  border: 1px black solid;
+  border-radius: 10px;
+  cursor: pointer;
+}
+ 
+
 
 </style>
